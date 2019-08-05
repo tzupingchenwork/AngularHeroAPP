e989fbda79025b538ca6bc687b0de05c59a6fb38
Andy <>
2019-08-05 11:20:43 +0800

    service

---
+++ src/app/hero.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+
+  // getHeroes(): Hero[] {
+  //   return HEROES;
+  // }
+  // 引入rxjs後
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,6 +3,9 @@ import { Hero } from '../hero';
 
 import { HEROES } from '../mock-heroes';
 
+import { HeroService } from '../hero.service';
+
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -13,16 +16,27 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Thor'
   };
-  heroes = HEROES;
+  // heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
-  constructor() { }
+  constructor( private heroService: HeroService) {}
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+  // 引入rxjs後
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
